4ec1932612162478620d38e66a2f4cf844a6bdb5
Sho Nihei <shonihei@gmail.com>
2019-04-28 20:14:42 -0400

    refactored endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,40 +1,29 @@
-const express = require('express')
-const fs = require('fs')
-const router = express.Router()
-const showdown = require('showdown')
-const { google } = require('googleapis')
-const oauth2Client = require('../oauth2-client')
-const expressJwt = require('express-jwt')
-const User = require('../models/user')
+const express = require('express');
+const fs = require('fs');
+const { google } = require('googleapis');
 
-const RSA_PUBLIC_KEY = fs.readFileSync('keys/jwtRS256.key.pub')
+const router = express.Router();
+const showdown = require('showdown');
 
-const converter = new showdown.Converter()
-const checkIfAuthenticated = expressJwt({
-  secret: RSA_PUBLIC_KEY,
-})
+const { checkIfAuthenticated, createOauth2Client } =
+  require('../middlewares/auth-middleware');
+
+const converter = new showdown.Converter();
 
 /* GET home page. */
 router.get('/', (_, res) => {
-  fs.readFile(`docs/endpoints.md`, 'utf-8', (err, data) => {
+  fs.readFile('docs/endpoints.md', 'utf-8', (err, data) => {
     if (err) throw err;
-    res.send(converter.makeHtml(data))
-  })
-})
+    res.send(converter.makeHtml(data));
+  });
+});
 
-router.get('/list', checkIfAuthenticated, async (req, res) => {
-  console.log(req.user)
-  const user = await User.findById(req.user.sub)
-  oauth2Client.setCredentials({
-    access_token: user.tokens.accessToken,
-    refresh_token: user.tokens.refresh_token,
-    scope: user.tokens.scope,
-    token_type: user.token_type,
-    expiry_date: user.expiry_date,
-  })
-  const calendar = google.calendar({ version: 'v3', auth: oauth2Client })
-  const list = await calendar.calendarList.list()
-  res.json(list)
-})
+router.get('/list', checkIfAuthenticated, createOauth2Client,
+  async (req, res) => {
+    const calendar = google.calendar({ version: 'v3', auth: req.oauth2Client });
 
-module.exports = router
+    const list = await calendar.calendarList.list();
+    res.json(list);
+  });
+
+module.exports = router;
